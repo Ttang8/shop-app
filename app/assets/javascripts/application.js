43ad25bb63933dfd4a40736784fad2d75532ce63
Terrence Tang <thtang28@gmail.com>
2018-09-26 02:43:43 -0700

    add skeleton for frontend

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,4 @@
 //= require rails-ujs
 //= require_tree .
 //= require jquery
-//= require jquery_ujss
+//= require jquery_ujs
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:show]
+    resource :session, only: [:create, :destroy, :show]
   end
 end
