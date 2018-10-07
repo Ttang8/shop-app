80825a89d3dd9714a750d1afbdd30a46fec2f81d
Terrence Tang <thtang28@gmail.com>
2018-10-06 22:49:13 -0700

    add create method for items controller

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,10 +1,15 @@
 class Api::ItemsController < ApplicationController
   def create
-
+    @item = Item.new(item_params)
+    if @item.save
+      render "api/items/show"
+    else
+      render json: @item.errors.full_messages, status: 422
+    end
 
   end
 
   def item_params
-    params.require(:item).permit()
+    params.require(:item).permit(:name, :item_number, :description, :image_url, :product_info, :price, :category_id)
   end
 end
